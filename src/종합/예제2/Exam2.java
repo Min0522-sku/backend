4bd5aad365e055dcd2f079d28bf13dfb2dbea5a2
Min0522-sku <wkdalstj0522@gmail.com>
2026-01-26 13:19:28 +0900

    종합예제2

---
+++ src/종합/예제2/Exam2.java
@@ -0,0 +1,43 @@
+package 종합.예제2;
+
+import java.util.Scanner;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] contents = new String[2];
+        String[] writers = new String[2];
+
+        for(;;){
+
+            System.out.println("==========My Community==========");
+            System.out.println("1.게시물 쓰기 | 2.게시물 출력");
+            System.out.println("================================");
+            int ch = scan.nextInt();
+            if (ch == 1){
+                scan.nextLine();
+                System.out.print("내용 : ");
+                String content = scan.nextLine();
+                System.out.print("작성자 : ");
+                String writer = scan.nextLine();
+                if(contents[contents.length-1] !=null){
+                    System.out.println("[경고] 게시물을 등록할 공간이 부족합니다.");
+                }
+                for (int i = 0; i < contents.length; i++){
+                    if(contents[i] == null){
+                        contents[i] = content;
+                        writers[i] = writer;
+                        break;
+                    }
+                }
+            } else if (ch == 2) {
+                for (int i = 0; i < contents.length; i++){
+                    if(contents[i] != null){
+                        System.out.printf("작성자: %s , 내용: %s \n", writers[i], contents[i]);
+                    }
+                }
+
+            }
+        }
+    }
+}
